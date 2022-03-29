7432a643b92e76c7ca0482b77e0ef7313707c12c
KateGordin <kate.gordin@gmail.com>
2022-03-29 11:34:00 +0200

    Redux slices

---
+++ src/components/PizzaList.js
@@ -0,0 +1,20 @@
+// src/components/PizzaList.js
+import { useSelector } from "react-redux";
+import { selectUser } from "../store/user/selectors";
+import { selectPizzas } from "../store/pizzas/selectors";
+
+export default function PizzaList() {
+  const user = useSelector(selectUser);
+  const pizzas = useSelector(selectPizzas);
+
+  return (
+    <div>
+      <h1>Pizza Explorer</h1>
+      <p>
+        Welcome back, <strong>{user.name}</strong>!
+      </p>
+      <p>There are {pizzas.length} in total</p>
+      <p>TODO: the list of pizzas</p>
+    </div>
+  );
+}
